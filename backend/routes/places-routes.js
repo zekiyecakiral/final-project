b86925bb833dd74e1c050411b9b9b816e963964e
zekiyecakiral <zekiyecakiral@gmail.com>
2020-09-18 22:55:30 +0200

    fullstack project

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const { check } = require('express-validator');
 
 const placesControllers = require('../controllers/places-controllers');
+const fileUpload = require('../middleware/file-upload');
+const checkAuth = require('../middleware/check-auth');
 
 const router = express.Router();
 
@@ -9,18 +11,31 @@ router.get('/:pid', placesControllers.getPlaceById);
 
 router.get('/user/:uid', placesControllers.getPlacesByUserId);
 
+router.use(checkAuth);
+
 router.post(
   '/',
+  fileUpload.single('image'),
   [
-    check('title').not().isEmpty(),
-    check('description').isLength({ min: 5 }, check('address').not().isEmpty()),
+    check('title')
+      .not()
+      .isEmpty(),
+    check('description').isLength({ min: 5 }),
+    check('address')
+      .not()
+      .isEmpty()
   ],
   placesControllers.createPlace
 );
 
 router.patch(
   '/:pid',
-  [check('title').not().isEmpty(), check('description').isLength({ min: 5 })],
+  [
+    check('title')
+      .not()
+      .isEmpty(),
+    check('description').isLength({ min: 5 })
+  ],
   placesControllers.updatePlace
 );
 
--- backend/util/location.js
+++ backend/util/location.js
@@ -2,38 +2,32 @@ const axios = require('axios');
 
 const HttpError = require('../models/http-error');
 
-const API_KEY =
-  'pk.eyJ1IjoiemVraXllIiwiYSI6ImNrZXNlNTQ2ZTNhdTIyeW5wYjhpcWR5OHAifQ.xvDhG3GwcfC6QL_yWjKr5Q';
+const API_KEY = process.env.MAPBOX_API_KEY;
 
 async function getCoordsForAddress(address) {
+  return {
+    lat: 40.7484474,
+    lng: -73.9871516
+  };
+  // const response = await axios.get(
+  //   `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
+  //     address
+  //   )}&key=${API_KEY}`
+  // );
 
-  
-  // return {
-  //   lat: 40.7484474,
-  //   lng: -73.9871516
-  // };
-  const response = await axios.get(
-    `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
-        address
-      )}.json?access_token=${API_KEY}`
-  );
-console.log(response.data);
-  const data = response.data;
+  // const data = response.data;
 
-  if (!data || data.status === 'ZERO_RESULTS') {
-    const error = new HttpError(
-      'Could not find location for the specified address.',
-      422
-    );
-    throw error;
-  }
+  // if (!data || data.status === 'ZERO_RESULTS') {
+  //   const error = new HttpError(
+  //     'Could not find location for the specified address.',
+  //     422
+  //   );
+  //   throw error;
+  // }
 
-  console.log('********');
-  console.log(data.features);
+  // const coordinates = data.results[0].geometry.location;
 
-  const coordinates = data.features[0].geometry.coordinates;
-
-  return coordinates;
+  // return coordinates;
 }
 
 module.exports = getCoordsForAddress;
